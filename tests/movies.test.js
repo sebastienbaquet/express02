d6fa24fb1131390d04cc3e9e01bbe11247830ee8
sebastien b <bas.envoi2604@gmail.com>
2023-10-24 22:31:38 +0200

    test complet

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -13,6 +13,14 @@ describe("GET /api/movies", () => {
     it("should return all movies", async () => {
         const response = await request(app).get('/api/movies/1');
         expect(response.headers["content-type"]).toMatch(/json/);
-        expect(response.status).toEqual(200);
+        expect(response.status).toEqual(200);  
     });
   });
+  describe("GET /api/movies/:id", () => {
+    it("should return all movies", async () => {
+        const response = await request(app).get('/api/movies/0');
+      
+        expect(response.status).toEqual(404);  
+    });
+  });
+  
